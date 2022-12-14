6b87e4c6e0ad71f7554c8407aae273a9d54d72f0
alainlou <az2lou@uwaterloo.ca>
2022-12-13 19:32:03 -0500

    solve day 5

---
+++ 5.1.hs
@@ -0,0 +1,30 @@
+import Data.Char (ord)
+import Data.List (transpose)
+import Data.List.Split (chunksOf, splitOn, splitWhen)
+
+isUpperChar c = ord c >= 65 && ord c <= 90
+parseState stateStr = (map (filter (isUpperChar)) . transpose . map (map (!!1) . chunksOf 4)) stateStr
+
+isNumber str = all (\c -> ord c >= 48 && ord c <= 57) str
+parseMove moveStr = ( map (read :: String -> Int) . filter (isNumber) . splitOn " ") moveStr
+
+evalMove currState [n, from, to] =
+    let
+        toMove = (reverse . take n) (currState !! (from - 1))
+    in
+    map (\(i, x) ->
+        if i == from
+        then drop n x
+        else if i == to
+        then toMove ++ x
+        else x)
+    $ zip [1..] currState
+solve startState = foldl evalMove startState
+
+main = do
+    input <- getContents
+    let [startStateStr, moves] = splitWhen null $ lines input
+    let startState = parseState startStateStr
+    let moveList = map parseMove moves
+    let finalState = solve startState moveList
+    putStrLn $ map (head) finalState
